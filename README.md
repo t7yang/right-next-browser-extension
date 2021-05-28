31f17354eb082a3d47a8ce977f82dabe7c1d53bc
t7yang <t7yang@gmail.com>
2021-05-28 12:49:07 +0800

    chore(release): 1.0.0

---
+++ README.md
@@ -0,0 +1,44 @@
+# Right Next Browser Extension
+
+Right Next browser extension make new tab place right next to current tab.
+
+Download: [Mozilla Addons](https://addons.mozilla.org/firefox/addon/right-next/)
+
+## Advantages
+
+Right Next can handle these situations:
+
+- Open new tab in foreground/left click.
+- Open new tab in background/middle click.
+- Open new tab from pinned tab.
+- Open new tab from/to [container](https://support.mozilla.org/en-US/kb/containers).
+
+Right Next also done with very less code which make it has good performance.
+
+## Permission Required
+
+NO.
+
+## End Support
+
+When Mozilla fixed this [issue](https://bugzilla.mozilla.org/show_bug.cgi?id=1485683), then this extension has its mission done.
+
+## Alternative
+
+If you don't open new tab by middle click, you can simply change these settings:
+
+```
+browser.tabs.insertAfterCurrent: true
+browser.tabs.insertRelatedAfterCurrent: false
+```
+
+These extensions work well too, but can't handle all situations.
+
+- [Open Tabs Next to Current](https://addons.mozilla.org/firefox/addon/open-tabs-next-to-current/) - can't handle container tab.
+- [Always Right](https://addons.mozilla.org/en-US/firefox/addon/always-right/) - can't handle middle click because it simply change your browser settings which affect by the issue mention above.
+
+Note: Observed on 2021-05-28.
+
+## File a Issue
+
+If you find any unexpected behaviour, you can file a issue [here](https://github.com/t7yang/right-next-browser-extension/issues).
---
+++ src/background.js
@@ -0,0 +1,44 @@
+(async function rightNext() {
+  /**
+   * @type {Map<number, {tabId: number, previousTabId: number | undefined, windowId: number}>}
+   */
+  const activeTabs = new Map();
+
+  /** intial process, save activate tab for each windows */
+  browser.tabs.query({ active: true }).then(tabs =>
+    tabs.forEach(tab =>
+      activeTabs.set(tab.windowId, {
+        tabId: tab.id,
+        windowId: tab.windowId,
+        previousTabId: browser.tabs.TAB_ID_NONE,
+      }),
+    ),
+  );
+
+  /** handler: update latest activate tab */
+  browser.tabs.onActivated.addListener(tab => {
+    activeTabs.set(tab.windowId, tab);
+  });
+
+  /** handler: rearrange new create tab  */
+  browser.tabs.onCreated.addListener(tab => {
+    const activeTab = activeTabs.get(tab.windowId);
+    return Promise.resolve(tab.openerTabId ?? activeTab.tabId)
+      .then(leftId =>
+        browser.tabs
+          .get(leftId)
+          // container will immediately kill the new tab and create a new container tab
+          // if the left tab's status is loading, drop and get previous active tab
+          .then(tab => (tab.status === 'loading' ? browser.tabs.get(activeTab.previousTabId) : tab)),
+      )
+      .then(tab =>
+        tab.pinned // if left tab is a pinned tab, need to find the currect insert position
+          ? browser.windows
+              .get(tab.windowId, { populate: true })
+              .then(win => win.tabs.findIndex((_, idx, arr) => !arr[idx + 1]?.pinned) + 1)
+          : tab.index + 1,
+      )
+      .then(index => browser.tabs.move(tab.id, { index }))
+      .catch(console.error);
+  });
+})();
