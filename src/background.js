60a87fc341fab2bdb7a4b52b3bbcee80b3cb3342
t7yang <t7yang@gmail.com>
2021-06-01 00:15:18 +0800

    perf: handle `tab.index > 1`  only to improve performance

--- src/background.js
+++ src/background.js
@@ -23,22 +23,23 @@
   /** handler: rearrange new create tab  */
   browser.tabs.onCreated.addListener(tab => {
     const activeTab = activeTabs.get(tab.windowId);
-    return Promise.resolve(tab.openerTabId || activeTab.tabId)
-      .then(leftId =>
-        browser.tabs
-          .get(leftId)
-          // container will immediately kill the new tab and create a new container tab
-          // if the left tab's status is loading, drop and get previous active tab
-          .then(tab => (tab.status === 'loading' ? browser.tabs.get(activeTab.previousTabId) : tab)),
-      )
-      .then(tab =>
-        tab.pinned // if left tab is a pinned tab, need to find the currect insert position
-          ? browser.windows
-              .get(tab.windowId, { populate: true })
-              .then(win => win.tabs.findIndex((_, idx, arr) => !arr[idx + 1]?.pinned) + 1)
-          : tab.index + 1,
-      )
-      .then(index => browser.tabs.move(tab.id, { index }))
-      .catch(console.error);
+    tab.index > 1 && // ignore 0 and 1, 0: the only tab in window; 1: new tab beside 1st tab
+      Promise.resolve(tab.openerTabId || activeTab.tabId)
+        .then(leftId =>
+          browser.tabs
+            .get(leftId)
+            // container will immediately kill the new tab and create a new container tab
+            // if the left tab's status is loading, drop and get previous active tab
+            .then(tab => (tab.status === 'loading' ? browser.tabs.get(activeTab.previousTabId) : tab)),
+        )
+        .then(tab =>
+          tab.pinned // if left tab is a pinned tab, need to find the currect insert position
+            ? browser.windows
+                .get(tab.windowId, { populate: true })
+                .then(win => win.tabs.findIndex((_, idx, arr) => !arr[idx + 1]?.pinned) + 1)
+            : tab.index + 1,
+        )
+        .then(index => browser.tabs.move(tab.id, { index }))
+        .catch(console.error);
   });
 })();
